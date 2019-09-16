6d393f5e3fe98b3953d29c8c19f323b956568243
Will Smith <lol.tihan@gmail.com>
2019-09-16 04:11:29 -0700

    work

--- tools/Vulkan.Generator/Program.fs
+++ tools/Vulkan.Generator/Program.fs
@@ -4,7 +4,12 @@ open System.Xml.Linq
 
 open FSharp.Data
 
-type VkEnumCase = VkEnumCase of name: string * value: int * comment: string option
+[<RequireQualifiedAccess>]
+type VkEnumCaseValue =
+    | UInt32 of uint32
+    | Ext of name: string * value: string
+
+type VkEnumCase = VkEnumCase of name: string * value: VkEnumCaseValue * comment: string option
 
 type VkMember = VkMember of name: string * typeName: string * isMutable: bool * comment: string option
 
@@ -20,9 +25,13 @@ type VkTypeKind =
 
 and VkType = VkType of name: string * comment: string option * VkTypeKind
 
-type VkBinding = VkBinding of name: string * value: string * comment: string option 
+type VkBinding = 
+    | VkBinding of name: string * value: string * comment: string option * obsoleteDescription: string option
+    | VkExtension of VkBinding * extends: string
 
-type VkCommand = VkCommand of name: string * parTypes: string list * returnType: string
+type VkParam = VkParam of name: string * typeName: string
+
+type VkFunction = VkFunction of name: string * pars: VkParam list * returnType: string * comment: string option
 
 type Vk = XmlProvider<"vk.xml">
 let vk = Vk.Load("vk.xml")
@@ -31,6 +40,8 @@ type env =
     {
         types: Map<string, VkType>
         primTypes: Map<string, string>
+
+        extEnums: Map<string, VkEnumCase list>
     }
 
 let rec getFixedSize env = function
@@ -62,21 +73,40 @@ let rec getFixedSize env = function
             |> List.max
         | VkType(_, _, VkFuncPointer _) -> failwith "Function pointers can't get a fixed size."
 
+let filterComment comment =
+    match comment with
+    | Some comment -> sprintf "/// %s\n" comment
+    | _ -> String.Empty
+
 let tryGetVkEnumCase (vkXmlEnum: Vk.Enum) =
-    match vkXmlEnum.Value.Number with
+    let valueOpt = 
+        if vkXmlEnum.XElement.FirstAttribute.Name.LocalName = "value" then
+            Some vkXmlEnum.XElement.FirstAttribute.Value
+        else
+            None
+
+    match valueOpt with
     | Some value ->
-        VkEnumCase(vkXmlEnum.Name, value, vkXmlEnum.Comment)
+        VkEnumCase(vkXmlEnum.Name, VkEnumCaseValue.UInt32(uint32 (int value)), vkXmlEnum.Comment)
         |> Some
     | _ ->
 
     match vkXmlEnum.Bitpos with
     | Some bitpos ->
-        VkEnumCase(vkXmlEnum.Name, 1 <<< bitpos, vkXmlEnum.Comment)
+        VkEnumCase(vkXmlEnum.Name, VkEnumCaseValue.UInt32(1u <<< bitpos), vkXmlEnum.Comment)
         |> Some
     | _ ->
         None
 
-let tryGetVkEnum (vkXmlEnums: Vk.Enums) =
+let getExtendedVkEnumCases env cases name =
+    match env.extEnums.TryFind name with
+    | Some cases2 -> cases @ cases2
+    | _ -> cases
+    |> List.sortByDescending (function VkEnumCase(value=value) -> value)
+    |> List.distinctBy (function VkEnumCase(name=name) -> name)
+    |> List.sortBy (function VkEnumCase(value=value) -> value)
+
+let tryGetVkEnum env (vkXmlEnums: Vk.Enums) =
     if vkXmlEnums.Type.IsNone then None
     else
         
@@ -86,6 +116,8 @@ let tryGetVkEnum (vkXmlEnums: Vk.Enums) =
             vkXmlEnums.Enums
             |> Seq.choose tryGetVkEnumCase
             |> List.ofSeq
+
+        let cases = getExtendedVkEnumCases env cases vkXmlEnums.Name
 
         if not cases.IsEmpty then
             VkType(vkXmlEnums.Name, vkXmlEnums.Comment, VkEnum cases)
@@ -98,6 +130,8 @@ let tryGetVkEnum (vkXmlEnums: Vk.Enums) =
             |> Seq.choose tryGetVkEnumCase
             |> List.ofSeq
 
+        let cases = getExtendedVkEnumCases env cases vkXmlEnums.Name
+
         if not cases.IsEmpty then
             VkType(vkXmlEnums.Name, vkXmlEnums.Comment, VkFlags cases)
             |> Some
@@ -106,20 +140,34 @@ let tryGetVkEnum (vkXmlEnums: Vk.Enums) =
     | _ ->
         None
 
+let rec isPointer (node: XNode) =
+    if node <> null then
+        match node.NodeType with
+        | XmlNodeType.Element ->
+            let node = node.NextNode
+            if node <> null && node.NodeType = XmlNodeType.Text then        
+                let node = node :> obj :?> XText
+                node.Value.Contains "*"
+            else
+                false
+        | XmlNodeType.Text ->
+            let node = node :> obj :?> XText
+            if node.Value.Contains "*" then
+                true
+            else
+                isPointer (node.NextNode)
+        | _ -> 
+            isPointer (node.NextNode)
+    else
+        false
+
 let getTypeName env typeName (node: XNode) =
     let typeName =
         match env.primTypes.TryFind typeName with
         | Some typeName -> typeName
         | _ -> typeName
         
-    let isPointer = 
-        match node.NodeType with
-        | XmlNodeType.Text -> 
-            (node :> obj :?> XText).Value.Contains "*"
-        | XmlNodeType.Element ->
-            (node :> obj :?> XElement).Value.Contains "*"
-        | _ ->
-            false
+    let isPointer = isPointer node
 
     match typeName, isPointer with
     | "void", true -> "nativeint"
@@ -129,7 +177,7 @@ let getTypeName env typeName (node: XNode) =
 let getVkMember env (vkXmlMember: Vk.Member) =
     let typeName =
         let typeName = vkXmlMember.Type
-        getTypeName env typeName vkXmlMember.XElement
+        getTypeName env typeName vkXmlMember.XElement.FirstNode
 
     let isMutable = not (vkXmlMember.XElement.Value.Contains "const")
 
@@ -260,6 +308,26 @@ let tryGetVkType env (vkXmlType: Vk.Type) =
     | _ ->
         None, env
 
+let tryMkVkBinding name aliasOpt commentOpt (xel: XElement) =
+    let valueOpt = 
+        if xel.FirstAttribute.Name.LocalName = "value" then
+            Some xel.FirstAttribute.Value
+        else
+            None
+    match valueOpt, aliasOpt with
+    | Some value, None -> 
+        let value =
+            // special cases
+            match value with
+            | "(~0U)" -> "~~~0u"
+            | "(~0ULL)" -> "~~~0UL"
+            | "(~0U-1)" -> "~~~0u-1u"
+            | "(~0U-2)" -> "~~~0u-2u"
+            | _ -> value
+        VkBinding(name, value, commentOpt, None) |> Some
+    | None, Some alias -> VkBinding(name, alias, commentOpt, None) |> Some
+    | _ -> None
+
 let getVkBindings () =
     vk.Enums
     |> Seq.map (fun x ->
@@ -267,24 +335,7 @@ let getVkBindings () =
         | _, None ->
             x.Enums
             |> Seq.choose (fun x ->
-                let valueOpt = 
-                    if x.XElement.FirstAttribute.Name.LocalName = "value" then
-                        Some x.XElement.FirstAttribute.Value
-                    else
-                        None
-                match valueOpt, x.Alias with
-                | Some value, None -> 
-                    let value =
-                        // special cases
-                        match value with
-                        | "(~0U)" -> "~~~0u"
-                        | "(~0ULL)" -> "~~~0UL"
-                        | "(~0U-1)" -> "~~~0u-1u"
-                        | "(~0U-2)" -> "~~~0u-2u"
-                        | _ -> value
-                    VkBinding(x.Name, value, x.Comment) |> Some
-                | None, Some alias -> VkBinding(x.Name, alias, x.Comment) |> Some
-                | _ -> None
+                tryMkVkBinding x.Name x.Alias x.Comment x.XElement
             )
             |> List.ofSeq
         | _ ->
@@ -293,12 +344,158 @@ let getVkBindings () =
     |> List.ofSeq
     |> List.concat
 
-let genVkEnumCase vkEnumCase =
+let getVkFunctions env =
+    vk.Commands.Commands
+    |> Seq.choose (fun x ->
+        match x.Proto with
+        | Some proto ->
+            let parTypes =
+                x.Params
+                |> Seq.map (fun p ->
+                    let typeName = getTypeName env p.Type p.XElement.FirstNode
+                    let name = p.Name
+                    // Special case this, we cannot have a param name of "type" or "module"
+                    let name = if name = "type" then "typ" elif name = "module" then "modul" else name
+                    VkParam(name, typeName)
+                )
+                |> List.ofSeq
+            let returnType = getTypeName env proto.Type x.XElement.FirstNode
+            VkFunction(proto.Name, parTypes, returnType, x.Comment) |> Some
+        | _ ->
+            None
+    )
+    |> List.ofSeq
+
+// TODO: Clean this up.
+let getVkBindingsForExtensions env =
+    let vkBindings =
+        vk.Features
+        |> Seq.map (fun x ->
+            x.Requires
+            |> Seq.map (fun y ->
+                y.Enums
+                |> Seq.choose (fun z ->
+                    match z.Offset, z.Extnumber with
+                    | Some offset, Some extnumber ->
+                        let dir =
+                            match z.Dir with
+                            | Some "-" -> -1
+                            | _ -> 1
+                        let value = dir * (1000000000 + ((extnumber - 1) * 1000) + offset)
+                        VkBinding(z.Name, string (uint32 value), z.Comment, None)
+                        |> Some
+                    | _ ->
+                        match z.Bitpos with
+                        | Some bitpos ->
+                            let value = 1u <<< bitpos
+                            VkBinding(z.Name, string value, z.Comment, None)
+                            |> Some
+                        | _ ->
+                            tryMkVkBinding z.Name z.Alias z.Comment z.XElement
+                    |> Option.map (fun vkBinding ->
+                        match z.Extends with
+                        | Some extends -> VkExtension(vkBinding, extends)
+                        | _ -> vkBinding
+                    )
+                )
+                |> List.ofSeq
+            )
+            |> List.ofSeq
+            |> List.concat
+        )
+        |> List.ofSeq
+        |> List.concat
+
+    let vkBindings2 =
+        vk.Extensions.Extensions
+        |> Seq.map (fun x ->
+            let obsoleteDescription =
+                match x.Obsoletedby with
+                | Some name -> sprintf "Use %s instead." name |> Some
+                | _ ->
+                    match x.Deprecatedby with
+                    | Some name -> sprintf "Use %s instead." name |> Some
+                    | _ -> None
+
+            x.Requires2
+            |> Seq.map (fun y ->
+                y.Enums
+                |> Seq.choose (fun z ->
+                    match z.Offset with
+                    | Some offset ->
+                        let dir =
+                            match z.Dir with
+                            | Some "-" -> -1
+                            | _ -> 1
+                        let value = dir * (1000000000 + ((x.Number - 1) * 1000) + offset)
+                        VkBinding(z.Name, string (uint32 value), z.Comment, None)
+                        |> Some
+                    | _ ->
+                        match z.Bitpos with
+                        | Some bitpos ->
+                            let value = 1u <<< bitpos
+                            VkBinding(z.Name, string value, z.Comment, None)
+                            |> Some
+                        | _ ->
+                            tryMkVkBinding z.Name z.Alias z.Comment z.XElement
+                    |> Option.map (fun vkBinding ->
+                        match z.Extends with
+                        | Some extends -> VkExtension(vkBinding, extends)
+                        | _ -> vkBinding
+                    )
+                )
+                |> List.ofSeq
+            )
+            |> List.ofSeq
+            |> List.concat
+        )
+        |> List.concat
+
+    let env =
+        (env, vkBindings @ vkBindings2)
+        ||> List.fold (fun env vkBinding ->
+            match vkBinding with
+            | VkExtension(VkBinding(name, value, comment, _), extends) -> 
+                let caseValue =
+                    match UInt32.TryParse value with
+                    | true, value -> VkEnumCaseValue.UInt32 value
+                    | _ -> VkEnumCaseValue.Ext (extends, value)
+                let case = VkEnumCase(name, caseValue, comment)
+                let extEnums =
+                    let cases =
+                        match env.extEnums.TryFind extends with
+                        | Some cases -> cases @ [case]
+                        | _ -> [case]
+                    env.extEnums
+                    |> Map.add extends cases
+                { env with extEnums = extEnums }
+            | _ -> 
+                env
+        )
+    vkBindings, env
+
+let genVkEnumCase env vkEnumCase =
     match vkEnumCase with
-    | VkEnumCase(name, value, Some comment) ->
-        sprintf "    /// %s\n    | %s = %i" comment name value
-    | VkEnumCase(name, value, _) ->
-        sprintf "    | %s = %i" name value
+    | VkEnumCase(name, value, comment) ->
+        let comment = filterComment comment
+        let value =
+            match value with
+            | VkEnumCaseValue.UInt32 value -> string value + "u"
+            | VkEnumCaseValue.Ext (extends, value) ->
+                let rec findDeepExt (extends: string) (value: string) = 
+                    let cases = env.extEnums.[extends]
+                    cases
+                    |> List.pick (fun x -> 
+                        match x with
+                        | VkEnumCase(name2, VkEnumCaseValue.Ext (extends2, value2), _) when value = name2 ->
+                            findDeepExt extends2 value2 |> Some
+                        | VkEnumCase(name2, VkEnumCaseValue.UInt32 value2, _) when value = name2 -> 
+                            string value2 + "u" |> Some
+                        | _ ->
+                            None
+                    )
+                findDeepExt extends value
+        "    " + comment + sprintf "    | %s = %s" name value
 
 let genVkMember vkMember =
     match vkMember with
@@ -358,13 +555,13 @@ let genVkType env vkType =
             match kind with
             | VkEnum cases ->
                 cases
-                |> List.map genVkEnumCase
+                |> List.map (genVkEnumCase env)
                 |> List.reduce (fun x y -> x + "\n" + y)
                 |> (+) "\n"
 
             | VkFlags cases ->
                 cases
-                |> List.map genVkEnumCase
+                |> List.map (genVkEnumCase env)
                 |> List.reduce (fun x y -> x + "\n" + y)
                 |> (+) "\n"
 
@@ -395,20 +592,52 @@ let genVkTypes env vkTypes =
     |> List.map (genVkType env)
     |> List.reduce (fun x y -> x + "\n\n" + y)
 
-let genVkBinding vkBinding =
+let tryGenVkBinding vkBinding =
     match vkBinding with
-    | VkBinding(name, value, comment) ->
-        let comment =
-            match comment with
-            | Some comment -> sprintf "/// %s\n" comment
+    | VkBinding(name, value, comment, obsoleteDescription) ->
+        let attribs =
+            match obsoleteDescription with
+            | Some x -> sprintf """[<Obsolete("%s")>]""" x + "\n"
             | _ -> String.Empty
-
-        sprintf "%slet %s = %s" comment name value
+        let comment = filterComment comment
+        sprintf "%s%slet %s = %s" comment attribs name value
+        |> Some
+    | _ ->
+        None
 
 let genVkBindings vkBindings =
     vkBindings
-    |> List.map genVkBinding
+    |> List.choose tryGenVkBinding
     |> List.reduce (fun x y -> x + "\n" + y)
+
+let filterParamAndReturnType typeName =
+    match typeName with
+    | "unit" -> "void"
+    | "nativeint" -> "void*"
+    | _ when typeName.StartsWith("nativeptr") ->
+        typeName.Replace("nativeptr<", String.Empty).Replace(">", String.Empty) + "&"
+    | _ -> typeName
+
+let genVkParams vkParams =
+    if List.isEmpty vkParams then
+        String.Empty
+    else
+        vkParams
+        |> List.map (function VkParam(name, typeName) -> filterParamAndReturnType typeName + " " + name)
+        |> List.reduce (fun x y -> x + ", " + y)
+
+let genVkFunction vkFunction =
+    match vkFunction with
+    | VkFunction(name, vkParams, returnType, comment) ->
+        filterComment comment + 
+        """[<DllImport("vulkan-1.dll", CallingConvention = CallingConvention.Winapi)>]""" + "\n" +
+        sprintf "extern %s %s" (filterParamAndReturnType returnType) name +
+        "(" + genVkParams vkParams + ")"
+
+let genVkFunctions vkFunctions =
+    vkFunctions
+    |> List.map genVkFunction
+    |> List.reduce (fun x y -> x + "\n\n" + y)
 
 let genSource () =
     let primTypes =
@@ -421,14 +650,16 @@ let genSource () =
             ("uint64_t", "uint64")
             ("int32_t", "int")
             ("int64_t", "int64")
-            ("size_t", "nativeint")          
+            ("size_t", "unativeint")          
         ]
 
-    let env = { types = Map.empty; primTypes = Map.ofList primTypes }
+    let env = { types = Map.empty; primTypes = Map.ofList primTypes; extEnums = Map.empty }
+
+    let vkBindingsForExtensions, env = getVkBindingsForExtensions env
 
     let vkEnumTypes =
         vk.Enums
-        |> Seq.choose tryGetVkEnum
+        |> Seq.choose (tryGetVkEnum env)
         |> List.ofSeq
 
     let vkTypes, env =
@@ -443,15 +674,18 @@ let genSource () =
         |> List.rev
 
     let vkBindings = getVkBindings()
+    let vkFunctions = getVkFunctions env
 
     "// File is generated. Do not modify.\n" +
-    "module rec FSharp.Vulkan\n\n" +
+    "module rec FSharp.Vulkan.Interop\n\n" +
     "open System\n" +
     "open System.Runtime.InteropServices\n\n" +
     """#nowarn "9" """ + "\n\n" +
     genVkTypes env vkEnumTypes + "\n\n" +
     genVkTypes env vkTypes + "\n\n" +
-    genVkBindings vkBindings
+    genVkBindings vkBindings + "\n\n" +
+    genVkFunctions vkFunctions + "\n\n" +
+    genVkBindings vkBindingsForExtensions
 
 open System.IO
 open System.Reflection
