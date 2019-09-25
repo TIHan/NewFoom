d0bf3cacad2af9a19eba5009afe290d544f08689
TIHan <lol.tihan@gmail.com>
2019-09-24 19:36:05 -0700

    Some spirv work

--- tools/FSharp.SpirV.Tests/Tests.fs
+++ tools/FSharp.SpirV.Tests/Tests.fs
@@ -1,9 +1,12 @@
 module Tests
 
 open System
+open System.IO
 open Xunit
-open FSharp.SpirV
+open FSharp.SpirV.Specification
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Deserialize triangle_vertex`` () =
+    use file = File.OpenRead("triangle_vertex.spv")
+    let spv = SPVModule.Deserialize(file)
+    Assert.Equal(MagicNumber, spv.MagicNumber)
