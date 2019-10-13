c943a707a94f3fdf8e19dc877aab89fb42b8b7c3
Will Smith <lol.tihan@gmail.com>
2019-10-13 07:41:16 -0700

    Added Falkan

---
+++ tools/Falkan/Device.fsi
@@ -0,0 +1,1 @@
+﻿module Falkan.Device
---
+++ tools/Falkan/Helpers.fs
@@ -0,0 +1,8 @@
+﻿[<AutoOpen>]
+module internal Falkan.Helpers
+
+open FSharp.Vulkan.Interop
+
+let checkResult result =
+    if result <> VkResult.VK_SUCCESS then
+        failwithf "%A" result
