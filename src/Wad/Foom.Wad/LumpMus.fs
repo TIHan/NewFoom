62df315434f6ca8df827c5257f45c7d65bed896d
Will Smith <lol.tihan@gmail.com>
2020-02-27 02:00:52 -0800

    work on mus

--- src/Wad/Foom.Wad/LumpMus.fs
+++ src/Wad/Foom.Wad/LumpMus.fs
@@ -8,17 +8,74 @@ open System
 
 type MusHeader =
     {
-        ScoreLength: int
-        ScoreStart: int
+        Length: int
+        Offset: int
         PrimaryChannelCount: int
         SecondaryChannelCount: int
         InstrumentCount: int
-        Instruments: int []
+        InstrumentPatches: int []
+    }
+
+type MidiController =
+    | BankSelect = 0uy
+    | Modulation = 1uy
+    | Volume = 7uy
+    | Pan = 10uy
+    | Expression = 11uy
+    | ReverbDepth = 91uy
+    | ChorusDepth = 93uy
+    | SustainPedalHold = 64uy
+    | SoftPedal = 67uy
+    | AllSoundsOff = 120uy
+    | AllNotesFadeOff = 123uy
+    | Mono = 126uy
+    | Poly = 127uy
+    | ResetAllControllersOnChannel = 121uy
+
+let midiController controllerNumber =
+    match controllerNumber with
+    | 0uy -> Unchecked.defaultof<MidiController>
+    | 1uy -> MidiController.BankSelect
+    | 2uy -> MidiController.Modulation
+    | 3uy -> MidiController.Volume
+    | 4uy -> MidiController.Pan
+    | 5uy -> MidiController.Expression
+    | 6uy -> MidiController.ReverbDepth
+    | 7uy -> MidiController.ChorusDepth
+    | 8uy -> MidiController.SustainPedalHold
+    | 9uy -> MidiController.SoftPedal
+    | _ -> failwithf "Invalid controller: %A" controllerNumber
+
+let systemEventToMidiController sysEvent =
+    match sysEvent with
+    | 10uy -> MidiController.AllSoundsOff
+    | 11uy -> MidiController.AllNotesFadeOff
+    | 12uy -> MidiController.Mono
+    | 13uy -> MidiController.Poly
+    | 14uy -> MidiController.ResetAllControllersOnChannel
+    | _ -> midiController sysEvent
+
+[<RequireQualifiedAccess>]
+type MusEvent =
+    | ReleaseNote of noteNumber: byte
+    | PlayNote of noteNumber: byte * volume: byte option
+    | PitchBend of amount: byte
+    | System of MidiController
+    | Controller of MidiController * value: byte
+    | EndOfMeasure
+    | Finish
+    | Unused
+
+type MusChannelEvent =
+    {
+        Channel: byte
+        Event: MusEvent
+        Delayed: byte []
     }
 
 type Mus =
     {
-        Events: int [] // placeholder
+        Events: MusChannelEvent []
     }
 
 let pHeader lumpHeader =
@@ -26,44 +83,106 @@ let pHeader lumpHeader =
         (
             u_pipe7 
                 (u_string 4)
-                u_uint16 // scoreLen
-                u_uint16 // scoreStart
-                u_uint16 // channels
-                u_uint16 // sec_channels
-                u_uint16 // instrCnt
-                u_uint16 // dummy
-            <| fun id scoreLen scoreStart channel secChannels instrCount _ ->
+                u_uint16
+                u_uint16
+                u_uint16
+                u_uint16
+                u_uint16
+                u_uint16
+            <| fun id length offset primaryChannelCount secondaryChannelCount instrumentCount _reserved ->
                 let isIdMus = id.StartsWith("mus", StringComparison.OrdinalIgnoreCase)
                 if not isIdMus then
                     failwith "Not a MUS lump."
                     
                 {
-                    ScoreLength = int scoreLen
-                    ScoreStart = int scoreStart
-                    PrimaryChannelCount = int channel
-                    SecondaryChannelCount = int secChannels
-                    InstrumentCount = int instrCount
-                    Instruments = [||]
+                    Length = int length
+                    Offset = int offset
+                    PrimaryChannelCount = int primaryChannelCount
+                    SecondaryChannelCount = int secondaryChannelCount
+                    InstrumentCount = int instrumentCount
+                    InstrumentPatches = [||]
                 }
         )
         >>= fun header ->
             u_array header.InstrumentCount u_uint16 
             |>> fun instruments -> 
-                { header with Instruments = instruments |> Array.map int }
+                { header with InstrumentPatches = instruments |> Array.map int }
     )
 
 // http://www.shikadi.net/moddingwiki/MUS_Format
 let pBody musHeader =
     u_lookAhead (
-        u_skipBytes (int64 musHeader.ScoreStart) >>.
+        u_skipBytes (int64 musHeader.Offset) >>.
         fun stream ->
-            let eventDescr = stream.ReadByte()
+            let mutable length = musHeader.Length
+            let events = ResizeArray()
+            let mutable prev = stream.Position
+            while length > 0 do
+                let eventDescr = stream.ReadByte()
 
-            let last = (eventDescr) >>> 7
-            let eventType = ((eventDescr) <<< 1) >>> 5
-            let channel = ((eventDescr <<< 4) >>> 4)
+                let last = (eventDescr) >>> 7
+                let eventType = ((eventDescr) <<< 1) >>> 5
+                let channel = ((eventDescr <<< 4) >>> 4)
 
-            { Events = [||] }
+                let event =
+                    match eventType with
+                    | 0uy ->
+                        let b = stream.ReadByte()
+                        let noteNumber = (b <<< 1) >>> 1
+                        MusEvent.ReleaseNote noteNumber
+                    | 1uy ->
+                        let b = stream.ReadByte()
+                        let volFlag = b >>> 7
+                        let noteNumber = (b <<< 1) >>> 1
+                        let volume =
+                            if volFlag <> 0uy then
+                                let b = stream.ReadByte()
+                                let volume = (b <<< 1) >>> 1
+                                Some volume
+                            else
+                                None
+                        MusEvent.PlayNote(noteNumber, volume)
+                    | 2uy ->
+                        let amount = stream.ReadByte()
+                        MusEvent.PitchBend amount
+                    | 3uy ->
+                        let b = stream.ReadByte()
+                        let sysEvent = (b <<< 1) >>> 1 >>> 2
+                        MusEvent.System(systemEventToMidiController sysEvent)
+                    | 4uy ->
+                        let b = stream.ReadByte()
+                        let controllerNumber = (b <<< 1) >>> 1 >>> 2
+                        let b = stream.ReadByte()
+                        let value = (b <<< 1) >>> 1
+                        MusEvent.Controller(midiController controllerNumber, value)
+                    | 5uy ->
+                        MusEvent.EndOfMeasure
+                    | 6uy ->
+                        MusEvent.Finish
+                    | 7uy ->
+                        MusEvent.Unused
+                    | _ ->
+                        failwithf "Invalid mus event: %A" eventType
+
+                let delayed =
+                    if last <> 0uy then
+                        let delayed = ResizeArray()
+                        let mutable stop = false
+                        while not stop do
+                            let b = stream.ReadByte()
+                            if b &&& 0x80uy = 0uy then
+                                stop <- true
+                            delayed.Add b
+                        delayed.ToArray()
+                    else
+                        [||]
+
+                length <- length - int (stream.Position - prev)
+                prev <- stream.Position
+                events.Add { Channel = channel; Event = event; Delayed = delayed }
+
+
+            { Events = events.ToArray() }
     )
 
 let parse lumpHeader =
--- src/Wad/Foom.Wad/LumpMus.fsi
+++ src/Wad/Foom.Wad/LumpMus.fsi
@@ -4,9 +4,43 @@ module internal Foom.Wad.LumpMus
 open System.IO
 open Foom.Wad.Unpickle
 
+type MidiController =
+    | BankSelect = 0uy
+    | Modulation = 1uy
+    | Volume = 7uy
+    | Pan = 10uy
+    | Expression = 11uy
+    | ReverbDepth = 91uy
+    | ChorusDepth = 93uy
+    | SustainPedalHold = 64uy
+    | SoftPedal = 67uy
+    | AllSoundsOff = 120uy
+    | AllNotesFadeOff = 123uy
+    | Mono = 126uy
+    | Poly = 127uy
+    | ResetAllControllersOnChannel = 121uy
+
+[<RequireQualifiedAccess>]
+type MusEvent =
+    | ReleaseNote of noteNumber: byte
+    | PlayNote of noteNumber: byte * volume: byte option
+    | PitchBend of amount: byte
+    | System of MidiController
+    | Controller of MidiController * value: byte
+    | EndOfMeasure
+    | Finish
+    | Unused
+
+type MusChannelEvent =
+    {
+        Channel: byte
+        Event: MusEvent
+        Delayed: byte []
+    }
+
 type Mus =
     {
-        Events: int [] // placeholder
+        Events: MusChannelEvent []
     }
 
 val Parse : LumpHeader -> Stream -> Mus
