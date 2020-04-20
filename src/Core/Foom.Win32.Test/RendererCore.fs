036bd3ab390af9fb87a9750243ede248330d0a1b
Will Smith <lol.tihan@gmail.com>
2020-04-20 01:24:47 -0700

    fixed it

--- src/Core/Foom.Win32.Test/RendererCore.fs
+++ src/Core/Foom.Win32.Test/RendererCore.fs
@@ -399,7 +399,7 @@ let updateLineView (sectorViews: Span<SectorView>) (lineView: inref<LineView>) =
                     Vector2(1.f, 0.f)
                 |]
 
-            lineView.FrontSide.Upper.PositionZ.Set(ReadOnlySpan positionZ)
+          //  lineView.FrontSide.Upper.PositionZ.Set(ReadOnlySpan positionZ)
             lineView.FrontSide.Upper.UV.Set(ReadOnlySpan uv)
 
         if lineView.FrontSide.HasMiddle then
@@ -452,7 +452,7 @@ type MapView(sectors: SectorView [], lineViews: LineView [], sectorRendersBuffer
         sectors.[sectorId] <- sectorView
     //    updateLineViews (Span sectors) (Span lineViews) sectorView.LineViewIds
         let v = { 
-            OriginalCeilingHeight = 5.f //sectorView.OriginalHeights.CeilingHeight
+            OriginalCeilingHeight = sectorView.OriginalHeights.CeilingHeight
             OriginalFloorHeight = sectorView.OriginalHeights.FloorHeight
             FloorHeight = heights.FloorHeight
             CeilingHeight = heights.CeilingHeight }
@@ -508,16 +508,20 @@ let load (graphics: FalGraphics) (wad: Wad) (map: Map) (mvpBuffer: VulkanBuffer<
                                 let height = top - bottom
                                 let origHeight = origTop - origBottom
 
-                                if origBottom = 5.f then
-                                    0.f
+                                if origHeight = 0.f then
+                                    height
                                 else
-                                   1.f // (height / origHeight)
+                                    (height / origHeight)
                             else
                                 1.f
 
-                        let mutable x = transform
+                        let mutable x = 1.f
+                        if uv.X = 1.f then
+                            x <- transform
 
                         let mutable y = 1.f
+                        if uv.Y = 1.f then
+                            y <- transform
 
 
                         gl_Position <- Vector4.Transform(Vector4(position, z * x, 1.f), mvp.model * mvp.view * mvp.proj)
@@ -584,8 +588,8 @@ let load (graphics: FalGraphics) (wad: Wad) (map: Map) (mvpBuffer: VulkanBuffer<
 
                     let x =
                         {
-                            FrontSideIndex = ldef.FrontSidedefIndex |> Option.map (fun _ -> sdef.SectorNumber) |> Option.defaultValue -1
-                            BackSideIndex = ldef.BackSidedefIndex |> Option.map (fun _ -> sdef.SectorNumber) |> Option.defaultValue -1
+                            FrontSideIndex = ldef.FrontSidedefIndex |> Option.map (fun i -> map.Sidedefs.[i].SectorNumber) |> Option.defaultValue -1
+                            BackSideIndex = ldef.BackSidedefIndex |> Option.map (fun i -> map.Sidedefs.[i].SectorNumber) |> Option.defaultValue -1
                         }
 
                     let upper =
